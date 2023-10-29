157037dfc328f72df32c892f3221107975773cc0
Vinicius Coradassi <viniciuscoradassi@hotmail.com>
2023-10-29 17:47:14 -0300

    Ajuste das funções

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 import threading
+import subprocess
 
 class DashboardController:
     def __init__(self):
@@ -33,6 +35,7 @@ class DashboardController:
         thread5.join()
         thread6.join()
         thread7.join()
+        self.dashApp.attInformacoes(self.dados)
         self.dashboard.after(5000, self.buscarDados)
 
 
@@ -43,30 +46,34 @@ class BuscaDados:
         self.quantidadeCPU = None
         self.infoHardware = None
         self.infoMemoria = None
+        self.memoriaResumo = None
         self.processos = None
         self.particoes = None
         self.infoSO = None
+        self.infoSO2 = None
 
     def buscaInformacoesCPU(self):
-        print('informacoesCPU')
+        self.informacoesCPU = subprocess.run(['car', '/proc/cpuinfo'], stdout=subprocess.PIPE)
 
     def buscaInfoMemoria(self):
-        print('Memoria')
+        self.memoriaResumo = subprocess.run(['free'], stdout=subprocess.PIPE)
+        self.infoMemoria = subprocess.run(['cat', '/proc/meminfo'], stdout=subprocess.PIPE)
 
     def buscaQuantidadeCPU(self):
-        print('quantidadeCPU')
+        self.quantidadeCPU = subprocess.run(['nproc'], stdout = subprocess.PIPE)
 
     def buscaInfoHardware(self):
-        print('infoHardware')
+        self.infoHardware = subprocess.run(['lscpu'], stdout = subprocess.PIPE)
 
     def buscaProcessos(self):
-        print('processos')
+        self.processos = subprocess.run(['ps', 'aux'], stdout = subprocess.PIPE)
 
     def buscaParticoes(self):
-        print('particoes')
+        self.particoes = subprocess.run(['cat', '/proc/partitions'], stdout = subprocess.PIPE)
 
     def buscaInfoSO(self):
-        print('Info SO')
+        self.infoSO = subprocess.run(["uname", '-a'], stdout=subprocess.PIPE)
+        self.infoSO2 = subprocess.run(['cat', '/proc/version'], stdout=subprocess.PIPE)
 
 #View
 class DashboardApp:
@@ -76,9 +83,25 @@ class DashboardApp:
         self.dashboard.geometry("960x540")
         self.dashboard.config(background="#eff5f6")
 
-        self.header = Frame(self.dashboard, bg='#333333')
-        self.header.place(x=0, y=0, width=960, height=60)
-        #self.iniciaTela()
+        self.frame1 = Frame(self.dashboard, bg='#eff5f6')
+        self.frame1.place(x=0, y=0, width=960, height=540)
+
+        nb = ttk.Notebook(self.dashboard)
+        nb.place(x=0, y=0)
+        nb.add(self.frame1, text="Aba 1")
+
+        self.frame2 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
+        nb.add(self.frame2, text="Aba 2")
+
+        self.frame3 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
+        nb.add(self.frame3, text="Aba 3")
+
+        self.frame4 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
+        nb.add(self.frame4, text="Aba 4")
+
+
+    def attInformacoes(self, dados):
+        print(dados.infoSO)
 
 
 if __name__ == "__main__":
