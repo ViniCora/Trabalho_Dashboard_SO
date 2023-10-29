bc2a8d146b9c0fb91c32d556502b19afa13d31f5
Vinicius Coradassi <viniciuscoradassi@hotmail.com>
2023-10-29 19:51:13 -0300

    Ajustes das telas

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import ttk
 import threading
 import subprocess
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 class DashboardController:
     def __init__(self):
@@ -50,10 +52,9 @@ class BuscaDados:
         self.processos = None
         self.particoes = None
         self.infoSO = None
-        self.infoSO2 = None
 
     def buscaInformacoesCPU(self):
-        self.informacoesCPU = subprocess.run(['car', '/proc/cpuinfo'], stdout=subprocess.PIPE)
+        self.informacoesCPU = subprocess.run(['cat', '/proc/cpuinfo'], stdout=subprocess.PIPE)
 
     def buscaInfoMemoria(self):
         self.memoriaResumo = subprocess.run(['free'], stdout=subprocess.PIPE)
@@ -73,7 +74,6 @@ class BuscaDados:
 
     def buscaInfoSO(self):
         self.infoSO = subprocess.run(["uname", '-a'], stdout=subprocess.PIPE)
-        self.infoSO2 = subprocess.run(['cat', '/proc/version'], stdout=subprocess.PIPE)
 
 #View
 class DashboardApp:
@@ -81,28 +81,111 @@ class DashboardApp:
         self.dashboard = dashboard
         self.dashboard.title("Dashboard Sistemas Operacionais")
         self.dashboard.geometry("960x540")
-        self.dashboard.config(background="#eff5f6")
-
-        self.frame1 = Frame(self.dashboard, bg='#eff5f6')
-        self.frame1.place(x=0, y=0, width=960, height=540)
+        self.teste = False
+        self.teste2 = False
 
         nb = ttk.Notebook(self.dashboard)
         nb.place(x=0, y=0)
-        nb.add(self.frame1, text="Aba 1")
+
+        self.frame1 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
+        nb.add(self.frame1, text="Info. SO")
 
         self.frame2 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
-        nb.add(self.frame2, text="Aba 2")
+        nb.add(self.frame2, text="Info. CPU")
 
         self.frame3 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
-        nb.add(self.frame3, text="Aba 3")
+        nb.add(self.frame3, text="Info. Hardware")
 
         self.frame4 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
-        nb.add(self.frame4, text="Aba 4")
+        nb.add(self.frame4, text="Info. Memoria")
+
+        nb2 = ttk.Notebook(self.frame4)
+        nb2.place(x=0, y=0)
+        self.frame4_1 = Frame(self.frame4, width=960, height=530, bg='#eff5f6')
+        nb2.add(self.frame4_1, text="Valores totais memória")
+        self.frame4_2 = Frame(self.frame4, width=960, height=530, bg='#eff5f6')
+        nb2.add(self.frame4_2, text="Gráfico memória")
+
+        self.frame5 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
+        nb.add(self.frame5, text="Info. processos")
+
+        nb3 = ttk.Notebook(self.frame5)
+        nb3.place(x=0, y=0)
+        self.frame5_1 = Frame(self.frame5, width=960, height=530, bg='#eff5f6')
+        nb3.add(self.frame5_1, text="Processos Lista")
+        self.frame5_2 = Frame(self.frame5, width=960, height=530, bg='#eff5f6')
+        nb3.add(self.frame5_2, text="Gráfico memória processos")
+
+        self.frame6 = Frame(dashboard, width=960, height=530, bg='#eff5f6')
+        nb.add(self.frame6, text="Info. partições")
+
+    def attTabelaMemoria(self, dados):
+        for widget in self.frame4_1.winfo_children():
+            widget.destroy()
+        table = ttk.Treeview(self.frame4_1, columns=(1, 2), show="headings", height=10)
+        table.pack()
+
+        table.heading(1, text="Tipo")
+        table.heading(2, text="Valor usado/disponivel (kb)")
+        table.insert('', tk.END, values=['Memoria Total', 2849964])
+        table.insert('', tk.END, values=['Memoria Usado', 2380868])
+        table.insert('', tk.END, values=['Memoria Livre', 82748])
+        table.insert('', tk.END, values=['Buff/Cache', 386348])
+        table.insert('', tk.END, values=['Memoria Compartilhada', 28852])
+        table.insert('', tk.END, values=['Memoria Disponivel', 273768])
 
 
     def attInformacoes(self, dados):
         print(dados.infoSO)
+        self.attGraficoMemoria(dados)
+        self.attGraficoUsoMemoriaProcessos(dados)
+        self.attTabelaMemoria(dados)
+        self.attInfoSO(dados)
 
+    def attInfoSO(self, dados):
+        for widget in self.frame1.winfo_children():
+            widget.destroy()
+        labelEXP = ttk.Label(self.frame1, text="Informações sobre o SO:")
+        label = "Linux ubuntu22 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux"
+        infoSOLabel = ttk.Label(self.frame1, text=label)
+        labelEXP.pack()
+        infoSOLabel.pack()
+
+    def attGraficoMemoria(self, dados):
+        for widget in self.frame4_2.winfo_children():
+            widget.destroy()
+        fig = Figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        labels = ['Usado', 'Livre', 'Buffer/Cache']
+        if self.teste2 == False:
+            sizes = [2380868, 82748, 386348]
+            self.teste2 = True
+        else:
+            sizes = [20, 35, 40]
+            self.teste2 = False
+
+        ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+        canvas = FigureCanvasTkAgg(fig, master=self.frame4_2)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+    def attGraficoUsoMemoriaProcessos(self, dados):
+        for widget in self.frame5_2.winfo_children():
+            widget.destroy()
+        fig = Figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        labels = ['Usado', 'Livre', 'Disponivel', 'Shared']
+        if self.teste == False:
+            sizes = [15, 30, 45, 10]
+            self.teste = True
+        else:
+            sizes = [20, 35, 40, 15]
+            self.teste = False
+
+        ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+        canvas = FigureCanvasTkAgg(fig, master=self.frame5_2)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
 
 if __name__ == "__main__":
     dash = DashboardController()
